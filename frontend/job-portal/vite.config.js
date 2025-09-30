6d4c3fc95cdd1be545c4fc00a2e2591d8bec0f83
mohamed fouda <mohamedfouda1810@gmail.com>
2025-09-30 06:10:01 +0300

    setup Tailwind CSS and postcss

--- frontend/job-portal/vite.config.js
+++ frontend/job-portal/vite.config.js
@@ -1,9 +1,10 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build',
+  plugins: [react(), tailwindcss()],
+    build: {
+    outDir: 'build', // مهم جداً للـ Vercel
   },
 })
